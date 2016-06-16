f94a4f2d163a826f89c7a81ee50cb8c2c0bd3186
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2016-06-16 15:17:28 +0200

    keep instances of commands in memory (instead of labels)

--- app/src/main/java/ohi/andre/consolelauncher/commands/CommandGroup.java
+++ app/src/main/java/ohi/andre/consolelauncher/commands/CommandGroup.java
@@ -3,7 +3,10 @@ package ohi.andre.consolelauncher.commands;
 import android.content.Context;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import ohi.andre.consolelauncher.tuils.Tuils;
@@ -11,35 +14,68 @@ import ohi.andre.consolelauncher.tuils.Tuils;
 public class CommandGroup {
 
     private String packageName;
-
-    private List<String> commands;
+    private CommandAbstraction[] commands;
+    private String[] commandNames;
 
     public CommandGroup(Context c, String packageName) {
         this.packageName = packageName;
 
+        List<String> cmds;
         try {
-            this.commands = Tuils.getClassesInPackage(packageName, c);
+            cmds = Tuils.getClassesInPackage(packageName, c);
         } catch (IOException e) {
+            return;
         }
 
-        Collections.sort(commands);
+        List<CommandAbstraction> cmdAbs = new ArrayList<>();
+        Iterator<String> iterator = cmds.iterator();
+        while (iterator.hasNext()) {
+            String s = iterator.next();
+            CommandAbstraction ca = buildCommand(s);
+            if(ca != null) {
+                cmdAbs.add(ca);
+            } else {
+                iterator.remove();
+            }
+        }
+
+        Collections.sort(cmds);
+        commandNames = new String[cmds.size()];
+        cmds.toArray(commandNames);
+        commands = new CommandAbstraction[cmdAbs.size()];
+        cmdAbs.toArray(commands);
     }
 
-    public CommandAbstraction getCommandByName(String name) throws Exception {
-        for (int count = 0; count < commands.size(); count++) {
-            String cmdName = commands.get(count);
-            if (!cmdName.equals(name))
-                continue;
-
-            String fullCmdName = packageName + "." + cmdName;
-            return Tuils.getCommandInstance(fullCmdName);
+    public CommandAbstraction getCommandByName(String name) {
+        for(CommandAbstraction c : commands) {
+            if(c.getClass().getName().endsWith(Tuils.DOT.concat(name))) {
+                return c;
+            }
         }
 
         return null;
     }
 
-    public List<String> getCommands() {
+    private CommandAbstraction buildCommand(String name) {
+        String fullCmdName = packageName + Tuils.DOT + name;
+        try {
+            Class<CommandAbstraction> clazz = (Class<CommandAbstraction>) Class.forName(fullCmdName);
+            if(CommandAbstraction.class.isAssignableFrom(clazz)) {
+                Constructor<CommandAbstraction> constructor = clazz.getConstructor();
+                return constructor.newInstance();
+            }
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public CommandAbstraction[] getCommands() {
         return commands;
+    }
+
+    public String[] getCommandNames() {
+        return commandNames;
     }
 
 }
