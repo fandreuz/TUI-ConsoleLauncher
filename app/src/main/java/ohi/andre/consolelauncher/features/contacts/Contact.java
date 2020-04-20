6eee08ffd85b50dd04bfb9bf80ef4a4a4938f61b
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2020-04-20 09:53:54 +0200

    create the standalone class Contact

--- app/src/main/java/ohi/andre/consolelauncher/features/contacts/Contact.java
+++ app/src/main/java/ohi/andre/consolelauncher/features/contacts/Contact.java
@@ -1,4 +1,41 @@
 package ohi.andre.consolelauncher.features.contacts;
 
-public class Contact {
+import android.support.annotation.NonNull;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Contact implements Comparable<Contact> {
+    // the full name of this contact
+    protected final String fullName;
+    
+    // the phone numbers which belong to this command.
+    // the default number is the first one
+    private List<String> phoneNumbers;
+    
+    public Contact (String name, List<String> numbers) {
+        this.fullName     = name;
+        this.phoneNumbers = numbers;
+    }
+    
+    protected String getPhoneNumber (int index) {
+        return phoneNumbers.get(index);
+    }
+    
+    protected int getPhoneNumbersCount() {
+        return phoneNumbers.size();
+    }
+    
+    protected String deletePhoneNumber(int index) {
+        return phoneNumbers.remove(index);
+    }
+    
+    protected List<String> getPhoneNumbers() {
+        return Collections.unmodifiableList(phoneNumbers);
+    }
+    
+    @Override
+    public int compareTo (@NonNull Contact o) {
+        return fullName.compareTo(o.fullName);
+    }
 }
